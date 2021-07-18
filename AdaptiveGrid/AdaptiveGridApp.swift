ed181995cabe1252dd5689d3de97bb8a5166748a
yovi.putra <yovi121093@gmail.com>
2021-07-18 11:59:57 +0700

    Initial Commit

---
+++ AdaptiveGrid/AdaptiveGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptiveGridApp.swift
+//  AdaptiveGrid
+//
+//  Created by Yovi Eka Putra on 18/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AdaptiveGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdaptiveGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdaptiveGrid
+//
+//  Created by Yovi Eka Putra on 18/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
